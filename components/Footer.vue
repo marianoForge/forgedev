515800dfa7affb1d412ebab0ca357912d550b4a1
Mariano De Simone <mariano@bigbangla.biz>
2020-08-21 17:01:33 -0600

    Se agrego la dirección en el footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,23 +3,31 @@
     <div class="container mx-auto">
       <div class="flex flex-wrap">
         <div class="w-full">
-          <h2 class="text-blue text-center mb-4 lg:mb-7">Let’s get in touch</h2>
+          <h2 class="text-blue text-center mb-12 lg:mb-7">Let’s get in touch</h2>
         </div>
         <div class="w-full lg:w-4/12"></div>
       </div>
+      <!-- Footer Nav -->
+      <footer-nav />
+      <!-- end Footer Nav-->
       <!-- Social networks -->
       <social-networks />
       <!-- end Social networks -->
+      <div class="text-center mt-8">
+        <p>201, Dev Arc, SG Road, Ahmedabad</p>
+        <p>380015, Gujarat, India</p>
+      </div>
     </div>
   </footer>
 </template>
 
 <script>
 import SocialNetworks from "~/components/Social-Networks.vue";
-
+import FooterNav from "~/components/Footer-Nav.vue";
 export default {
   components: {
     SocialNetworks,
-  },
+    FooterNav
+  }
 };
 </script>
