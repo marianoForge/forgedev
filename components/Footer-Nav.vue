c75e3b90eb904769c38c1c45ce3ca148240e1165
Mariano De Simone <mariano@bigbangla.biz>
2020-08-21 16:52:18 -0600

    Se crearon los files del footer

---
+++ components/Footer-Nav.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <ul class="flex items-center justify-between flex-wrap px-4 mb-20">
+      <div class="items-center flex-shrink-0 mr-6">
+        <li>
+          Build
+          <span class="arrow ml-3">></span>
+        </li>
+        <small>Help you to create something</small>
+      </div>
+      <div class="items-center flex-shrink-0 mr-6">
+        <li>
+          Co-incubate
+          <span class="arrow ml-3">></span>
+        </li>
+        <small>Co-incubate an idea together</small>
+      </div>
+      <div class="ftems-center flex-shrink-0 mr-6">
+        <li>
+          Build
+          <span class="arrow ml-3">></span>
+        </li>
+        <small>Help you to create something</small>
+      </div>
+      <div class="items-center flex-shrink-0 mr-6">
+        <li>
+          Co-incubate
+          <span class="arrow ml-3">></span>
+        </li>
+        <small>Co-incubate an idea together</small>
+      </div>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
