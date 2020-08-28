79cf9c56eaba1e63fa17a29b44a22fa631ca7382
Mariano De Simone <mariano@bigbangla.biz>
2020-08-27 23:27:05 -0600

    Se subiron los arcivos de forgeServices y se realizo la seccion Clock

---
+++ components/forgeServices/UXDesign.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="w-full lg:w-4/12 text-center pl-40">
+    <div class="flex service--wrapper flex-column z-10" data-aos="fade-up">
+      <!-- <div class="circle circle-yellow"></div> -->
+      <div class="service--info__wrapper" style="width:400px" ;>
+        <div class="service--info__off">
+          <h3 class="text-black">Research & Design</h3>
+          <ul>
+            <li>
+              <b>Services</b>
+            </li>
+            <li>Research & Analysis</li>
+            <li>UX/UI Design</li>
+            <li>Wireframes</li>
+            <li>Interface Designs</li>
+          </ul>
+          <ul>
+            <li>
+              <b>Technologies</b>
+            </li>
+            <li>Sketch</li>
+            <li>Adobe Suite</li>
+            <li>Invision</li>
+            <li>AxureAI</li>
+            <li>Prototype</li>
+            <li>AxureAI</li>
+            <li>Wireframes</li>
+          </ul>
+        </div>
+        <img
+          src="~/assets/images/forge-research-ui-ux-design.svg"
+          class="mx-auto shadow-svg image--service w-40"
+          alt="Research & Design"
+        />
+        <div class="service--info__on mt-10">
+          <h3 class="text-white text-xl">Research & Design</h3>
+          <p class="text-white font-thin text-0.5xl">Track your favorite shows</p>
+          <p class="text-white font-thin text-0.5xl">automatically without switching</p>
+          <p class="text-white font-thin text-0.5xl">between apps.</p>
+          <p class="pt-4">
+            <a href="#" style="color: #1bcac4">Learn More ></a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
